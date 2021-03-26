8ef6a0746f7e74e43e78a1cfb8463bdddcc81c81
Aloïs <aloisebrechts@gmail.com>
2021-03-26 13:50:18 +0100

    jegfv

--- libc/str/atoi/ft_atoi.c
+++ libc/str/atoi/ft_atoi.c
@@ -6,28 +6,34 @@
 /*   By: asebrech <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/26 11:47:58 by asebrech          #+#    #+#             */
-/*   Updated: 2021/03/26 12:02:32 by asebrech         ###   ########.fr       */
+/*   Updated: 2021/03/26 13:46:27 by asebrech         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-
 int	ft_atoi(const char *str)
 {
-	int i;
-	int sign;
-	int res;
+	int	i;
+	int	sign;
+	int	res;
 
 	i = 0;
-	while (str[i] == '\t' || str[i] == '\n' || str[i] == '\v' ||
-			str[i] == '\f' || str[i] == '\r' || str[i] == ' ')
+	while (str[i] == '\t' || str[i] == '\n' || str[i] == '\v' || str[i] == '\f'
+		|| str[i] == '\r' || str[i] == ' ')
 		i++;
 	sign = 1;
-	if (str[i] == '+' str[i] == '-')
+	if (str[i] == '+' || str[i] == '-')
 	{
 		if (str[i] == '-')
-			sign
+			sign *= -1;
 		i++;
 	}
+	res = 0;
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		res = res * 10 + (str[i] - '0');
+		i++;
+	}
+	return (res * sign);
 }
---
+++ libc/str/atoi/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "libft.h"
+
+int	main()
+{
+	char *str = " 	 	 -117fjw36   ";
+	
+	printf("or : %d\n", atoi(str));	
+	printf("ft : %d\n", ft_atoi(str));	
+}
